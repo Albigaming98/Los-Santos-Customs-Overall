ac8608aa43e54382c44a9559399c8fc9ecdb7b4e
Albigaming98 <91427364+Albigaming98@users.noreply.github.com>
2024-09-30 04:01:45 +0200

    Add files via upload

---
+++ lsc_uniform_4c/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_lsc_uniform_4c.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_lsc_uniform_4c.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
